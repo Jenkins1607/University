32e4ea0d1c33340c7779f322089477873d1394c0
Максим <smetanaka123@gmail.com>
2025-03-05 10:10:21 +0300

    Add files via upload
    Усложнённая распределительная задача. (alloc.py - упрощённая)
    Теперь есть потеря ресурса при производстве, регулируемая функциями H_i для каждой функции производства F_i

---
+++ PowerAlloc.py
@@ -0,0 +1,46 @@
+def allocation(Y, N, F, H):
+    
+    # таблица для хранения максимального объема продукции 
+    dp = [ [0 for _ in range(Y + 1)] for _ in range(N + 1)]
+    
+    for i in range(1, N + 1):
+        for y in range(Y + 1):
+            dp[i][y] = dp[i - 1][y]
+            
+            for x in range(y + 1):
+                if H[i-1](x) < y:
+                    dp[i][y] = max(dp[i][y], dp[i - 1][y - H[i - 1](x)] + F[i - 1](x))
+    
+    return dp
+
+def main():
+    while True:
+        N = 4 # Число предприятий 
+        Y = int(input("Введите кол-во единиц ресурса: ")) # Кол-во единиц некоторого ресурса
+
+        F = [
+        lambda x: x*15,
+        lambda x: x*12,
+        lambda x: x*14,
+        lambda x: x*22
+        ]
+
+        H = [
+        lambda x: 2*x,
+        lambda x: x,
+        lambda x: 3*x,
+        lambda x: 4*x
+        ]
+
+        res = allocation(Y, N, F, H)
+        print("Результат-Матрица: ", end="\n")
+        for row in res:
+            for elem in row:
+                print(str(elem).ljust(3), end=" ")
+            print()
+
+        print()
+
+        print(f"Максимальный объем продукции: {res[N][Y]}")
+
+main()
